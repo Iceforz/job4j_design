cacbf314dff4c1f010d65881e9cd24881a9a1968
iceforz <iceforz3@gmail.com>
2020-10-03 14:58:28 +0300

    2. Без переопределения equals и hashCode [#365800]

--- src/main/java/ru/job4j/hash/User.java
+++ src/main/java/ru/job4j/hash/User.java
@@ -1,9 +1,31 @@
 package ru.job4j.hash;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
 
 public class User {
    private String name;
    private int children;
    private Calendar birthday;
+
+   public User(String name, int children, Calendar birthday) {
+      this.name = name;
+      this.children = children;
+      this.birthday = birthday;
+   }
+
+   public static void main(String[] args) {
+      User one = new User("Ilya", 0, new GregorianCalendar(1997, 4, 30));
+      User two = new User("Ilya", 0, new GregorianCalendar(1997, 4, 30));
+      Set<User> map = new HashSet<>();
+      map.add(one);
+      map.add(two);
+      if (one.equals(two)) {
+         System.out.println(map);
+      } else {
+         System.out.println(123);
+      }
+   }
 }
