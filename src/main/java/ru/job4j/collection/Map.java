c645f0df07ea407651c75d5d42c38e0f28d382ed
iceforz <iceforz3@gmail.com>
2020-10-10 22:34:00 +0300

    8. Реализовать собственную структуру данных - HashMap [#365803]

--- src/main/java/ru/job4j/collection/Map.java
+++ src/main/java/ru/job4j/collection/Map.java
@@ -4,152 +4,6 @@ import java.util.*;
 
 public class Map<K, V> implements Iterable {
 
-  /*  private Node<K, V>[] table;
-    private int count = 0;
-    private int size = 0;
-    private int modCount = 0;
-    private float loadFactor = 0.75f;
-
-
-   static class Node<K, V> {
-        public K key;
-        public V value;
-
-        public Node(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public K getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public void setValue(V value) {
-            this.value = value;
-        }
-
-        @Override
-        public int hashCode() {
-            return 16 * key.hashCode() * value.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o instanceof Node) {
-                Node<K, V> node = (Node) o;
-                if (!Objects.equals(this.key, node.key)) {
-                    return false;
-                }
-                if (!Objects.equals(this.value, node.value)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
-    public int size() {
-        return this.size();
-    }
-
-    public boolean insert(K key, V value) {
-        if (count / table.length >= loadFactor) {
-            resize();
-        }
-        if (key != null) {
-            Node<K, V> element = new Node<>(key, value);
-            int index = getIndex(key.hashCode());
-            if (table[index] == null) {
-                table[index] = element;
-                count++;
-                modCount++;
-                size++;
-                return true;
-            } else if (table[index].getKey().equals(key)) {
-                table[index].setValue(value);
-            }
-        }
-        return false;
-    }
-
-    public V get(K key) {
-        Node<K, V> element = table[getIndex(key.hashCode())];
-        if (key.equals(element.getKey())) {
-            return element.getValue();
-        }
-        return null;
-    }
-
-    public boolean delete(K key) {
-        if (key != null) {
-            int index = getIndex(key.hashCode());
-            if (table[index].getKey().equals(key)) {
-                table[index] = null;
-                count--;
-                modCount++;
-                size--;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private int getIndex(int hash) {
-        return Math.abs(hash) % table.length;
-    }
-
-    public void resize() {
-        Node[] tempTable = table;
-        table = new Node[table.length * 2];
-        for (Node<K, V> node : tempTable) {
-            insert(node.getKey(), node.getValue());
-            count--;
-        }
-    }
-
-    @Override
-    public Iterator iterator() {
-        return new Iterator() {
-            private int cursor = 0;
-            private int expectedModCount = modCount;
-
-            @Override
-            public boolean hasNext() {
-                boolean result = false;
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                for (int i = cursor; i < table.length; i++) {
-                    if (table[i] != null) {
-                        result = true;
-                        cursor = i;
-                        break;
-                    }
-                }
-                return result;
-            }
-
-            @Override
-            public V next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                while (table[cursor] == null && cursor < table.length) {
-                    cursor++;
-                }
-                return (V) table[cursor++].getValue();
-            }
-        };
-    }
-}
-*/
-
     static final float loadFactor = 0.75f;
     private Node<K, V>[] table;
     private int capacity = 16;
@@ -188,14 +42,25 @@ public class Map<K, V> implements Iterable {
         return (capacity - 1) & hash(key);
     }
 
-    private void resize() {
-        if (table == null) {
-            table = (Node<K, V>[]) new Node[capacity];
-        } else {
-            capacity = capacity << 1;
-            table = Arrays.copyOf(table, capacity);
+    private Node<K, V>[] resize() {
+            Node<K, V>[] newTable;
+            if (table == null) {
+                table = (Node<K, V>[]) new Node[capacity];
+                newTable = (Node<K, V>[]) new Node[capacity];
+            } else {
+                capacity = capacity << 1;
+                table = Arrays.copyOf(table, capacity);
+                newTable = (Node<K, V>[]) new Node[capacity];
+                for (Node<K, V> node : table) {
+                    if (node != null) {
+                        int index = getIndex(node.getKey());
+                        newTable[index] = node;
+                    }
+                }
+            }
+            return newTable;
         }
-    }
+
 
     public boolean insert(K key, V value) {
         if (table == null || size > loadFactor * capacity) {
@@ -203,7 +68,7 @@ public class Map<K, V> implements Iterable {
         }
         Node<K, V> n = new Node<>(hash(key), key, value);
         int index = getIndex(key);
-        if (table[index] != null) {
+        if (table[index] != null && table[index].getKey().equals(key)) {
             return false;
         }
         table[index] = n;
@@ -215,7 +80,7 @@ public class Map<K, V> implements Iterable {
     public V get(K key) {
         int index = getIndex(key);
         int validatedIndex = Objects.checkIndex(index, size);
-        if (table[validatedIndex] == null) {
+        if (table[validatedIndex] == null && table[index].getKey().equals(key)) {
             return null;
         }
         return table[validatedIndex].getValue();
@@ -224,7 +89,7 @@ public class Map<K, V> implements Iterable {
     public boolean delete(K key) {
         int index = getIndex(key);
         int validatedIndex = Objects.checkIndex(index, size);
-        if (table[validatedIndex] == null) {
+        if (table[validatedIndex] == null && table[index].getKey().equals(key)) {
             return false;
         }
         table[validatedIndex] = null;
@@ -266,4 +131,3 @@ public class Map<K, V> implements Iterable {
         };
     }
 }
-
