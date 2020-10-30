4e3531ecdf23b7c5ce6ba25d7ef26d2f7ac9b772
iceforz <iceforz3@gmail.com>
2020-10-30 13:57:44 +0300

    8. Реализовать собственную структуру данных - HashMap [#365803]

---
+++ src/main/java/ru/job4j/collection/SimpleHashMap.java
@@ -0,0 +1,115 @@
+package ru.job4j.collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleHashMap<K, V> implements Iterable<SimpleHashMap.Node<K, V>> {
+        private static final float LOAD_FACTOR = 0.75f;
+        private Node<K, V>[] table;
+        private int size = 0;
+
+        public boolean insert(K key, V value) {
+            int start = size;
+                if (table == null || (table.length * LOAD_FACTOR) == size) {
+                    resize();
+                }
+                int hash = Objects.hashCode(key);
+                Node<K, V> node = new Node<>(key, value, hash);
+                add(node);
+                return start != size;
+            }
+
+            public V get(K key) {
+                int index = indexOf(key);
+                    if (table[index] != null && table[index].getKey().equals(key)) {
+                        return table[index].getValue();
+                    }
+                    return null;
+                }
+
+                public boolean delete(K key) {
+                    int start = size;
+                    int index = indexOf(key);
+                        if (table[index] != null && table[index].getKey().equals(key)) {
+                            table[index] = null;
+                            size--;
+                        }
+                        return start != size;
+                    }
+                    public int getSize() {
+                        return size;
+                    }
+
+                    private void add(Node<K, V> element) {
+                        int index = indexOf(element.getKey());
+                        if (table[index] == null) {
+                            table[index] = element;
+                            size++;
+                        }
+                    }
+                    private int indexOf(K key) {
+                        int hash = Objects.hashCode(key);
+                        return hash % table.length;
+                    }
+                    private void resize() {
+                        if (table == null) {
+                            table = new Node[16];
+                        } else {
+                            int newSize = table.length << 1;
+                            table = replaceItem(table, new Node[newSize]);
+                        }
+                    }
+                    private Node<K, V>[] replaceItem(Node<K, V>[] oldTab, Node<K, V>[] newTab) {
+                        for (int i = 0; i < oldTab.length; i++) {
+                            int index = oldTab[i].hash % newTab.length;
+                            newTab[index] = oldTab[i];
+                        }
+                        return newTab;
+                    }
+
+                        @Override
+                        public Iterator<Node<K, V>> iterator() {
+                            return new Iterator<Node<K, V>>() {
+                                private int point = 0;
+                                @Override
+                                public boolean hasNext() {
+                                    return point != size;
+                                }
+
+                                @Override
+                                public Node<K, V> next() {
+                                    if (!hasNext()) {
+                                        throw new NoSuchElementException();
+                                    }
+                                    Node<K, V> result = null;
+                                    for (int i = point; i < table.length; i++) {
+                                        if (table[i] != null) {
+                                            result = table[i];
+                                            point++;
+                                            break;
+                                        }
+                                    }
+                                    return result;
+                                }
+                            };
+                        }
+
+                        static class Node<K, V> {
+                            private K key;
+                            private V value;
+                            private int hash;
+                            public Node(K key, V value, int hash) {
+                                this.key = key;
+                                this.value = value;
+                                this.hash = hash;
+                            }
+
+                            public K getKey() {
+                                return key;
+                            }
+
+                            public V getValue() {
+                                return value;
+                            }
+        }
+}
---
+++ src/test/java/ru/job4j/collection/SimpleHashMapTest.java
@@ -0,0 +1,76 @@
+package ru.job4j.collection;
+import java.util.Iterator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class SimpleHashMapTest {
+
+    @Test
+    public void whenAddThenTrue() {
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
+        boolean result = map.insert("one", 1);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenAddThenSizeGrowUp() {
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
+        map.insert("one", 1);
+        assertThat(map.getSize(), is(1));
+    }
+    @Test
+    public void whenAdd2EqualsKeyThenFalse() {
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
+        map.insert("one", 1);
+        boolean result = map.insert("one", 2);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenAdd2ElementThenGetFirst() {
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
+        map.insert("one", 1);
+        map.insert("two", 2);
+        int result = map.get("one");
+        assertThat(result, is(1));
+    }
+    @Test
+    public void whenDeleteThenTrue() {
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
+        map.insert("one", 1);
+        map.insert("two", 2);
+        boolean result = map.delete("one");
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenDeleteThenSizeCountDown() {
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
+        map.insert("one", 1);
+        map.insert("two", 2);
+        map.delete("one");
+        int result = map.getSize();
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void whenAdd2ElementThenIteratorNextReturnOne() {
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
+        map.insert("one", 1);
+        map.insert("two", 2);
+        Iterator<SimpleHashMap.Node<String, Integer>> iterator = map.iterator();
+        int result = iterator.next().getValue();
+        assertThat(result, is(1));
+
+    }
+
+    @Test
+    public void whenAdd2ElementThenHasNextReturnTrue() {
+        SimpleHashMap<String, Integer> map = new SimpleHashMap<>();
+        map.insert("one", 1);
+        map.insert("two", 2);
+        Iterator<SimpleHashMap.Node<String, Integer>> iterator = map.iterator();
+        boolean result = iterator.hasNext();
+        assertThat(result, is(true));
+    }
+}
