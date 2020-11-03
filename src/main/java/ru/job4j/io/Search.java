1af0c67d407ea5e4657065d4ee419b0ec9bdfcc1
iceforz <iceforz3@gmail.com>
2020-11-03 13:49:43 +0300

    4.1. Сканирование файловой системы. [#365813]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -4,6 +4,9 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class Search {
     public static void main(String[] args) throws IOException {
@@ -12,26 +15,44 @@ public class Search {
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
-        SearchFiles searcher = new SearchFiles(ext);
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
 
-    static class SearchFiles extends SimpleFileVisitor<Path> {
-        private List<Path> pathsList = new ArrayList<>();
-        private String x;
-        SearchFiles(String ext) {
-            this.x = ext;
+    public static class SearchFiles implements FileVisitor<Path> {
+        private Predicate<Path> pathPredicate;
+        private List<Path> listOfPath = new ArrayList<>();
+
+        public SearchFiles(Predicate<Path> pathPredicate) {
+            this.pathPredicate = pathPredicate;
         }
+
+        public List<Path> getPaths() {
+            return this.listOfPath;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            return CONTINUE;
+        }
+
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            if (file.getFileName().toString().endsWith(x)) {
-                pathsList.add(file);
+            if (pathPredicate.test(file)) {
+                this.listOfPath.add(file);
             }
-            return super.visitFile(file, attrs);
+            return CONTINUE;
         }
-        public List<Path> getPaths() {
-            return pathsList;
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            return CONTINUE;
         }
     }
 }
